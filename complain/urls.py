1a4bb401f0908daa674a7a6586bf4bd9ecb3cde8
root <reshajullyantama01@gmail.com>
2019-03-08 01:47:29 +0700

    templating email

--- complain/urls.py
+++ complain/urls.py
@@ -1,7 +1,7 @@
-from django.urls import path
+from django.urls import path,re_path
 from . import views
 
 urlpatterns = [
 	path('',views.index, name='complain'),
-	path('email/',views.email)
+	path('<int:id>', views.email)
 ]
--- complain/views.py
+++ complain/views.py
@@ -1,4 +1,6 @@
+import ipaddress, IPy
 from django.shortcuts import render
+from django.http import HttpResponse
 from .models import complain_base
 
 from django.core.mail import send_mail
@@ -13,11 +15,37 @@ def index(request):
 	}
 	return render(request,'index.html',context)
 
-def email(request):
-	send_mail(
-			'Testing',
-			'Ayam',
-			'foolgum01@gmail.com',
-			['kambing2933@gmail.com'],
-			fail_silently=False,
-		)
+def email(request,id):
+	
+	context = {
+		'items': complain_base.objects.get(id=id)
+	}
+	items = complain_base.objects.get(id=id)
+		
+		# IP Address
+	test = items.ip_addr
+	ip_address = IPy.IP(test).strNormal()
+
+		#Kode Toko
+	kode_toko = items.description
+
+		#Customer_ID
+	sid = items.customer_id
+	if sid is not None:
+		sid
+	else:
+		sid = 'No SID'
+	return HttpResponse(ip_address+' '+kode_toko+' '+sid)
+	# body = '<table><tr><td>Kode Toko </td><td>:</td><td></td></tr></table>'
+	# print(id)
+	# try :
+	# 	send_mail(
+	# 			'Testing',
+	# 			'Ayam',
+	# 			'foolgum01@gmail.com',
+	# 			['kambing2933@gmail.com'],
+	# 			fail_silently=False,
+	# 		)
+	# 	return HttpResponse(body)
+	# except:
+	# 	return HttpResponse('email tidak terkirim')
