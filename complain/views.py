df1d44b3adc1fc95d4bfa9971a647489bb2c00e1
root <reshajullyantama01@gmail.com>
2019-03-14 00:12:44 +0700

    Email can Send

--- complain/views.py
+++ complain/views.py
@@ -2,6 +2,7 @@ import ipaddress, IPy
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import complain_base
+from django.template import loader
 
 from django.core.mail import send_mail
 # Create your views here.
@@ -26,6 +27,8 @@ def email(request,id):
 	test = items.ip_addr
 	ip_address = IPy.IP(test).strNormal()
 
+	ip_public = items.ip_public
+
 		#Kode Toko
 	kode_toko = items.description
 
@@ -35,17 +38,18 @@ def email(request,id):
 		sid
 	else:
 		sid = 'No SID'
-	return HttpResponse(ip_address+' '+kode_toko+' '+sid)
-	# body = '<table><tr><td>Kode Toko </td><td>:</td><td></td></tr></table>'
-	# print(id)
-	# try :
-	# 	send_mail(
-	# 			'Testing',
-	# 			'Ayam',
-	# 			'foolgum01@gmail.com',
-	# 			['kambing2933@gmail.com'],
-	# 			fail_silently=False,
-	# 		)
-	# 	return HttpResponse(body)
-	# except:
-	# 	return HttpResponse('email tidak terkirim')
+	# body = "<table>"ip_address+' '+kode_toko+' '+sid+"</table>"
+	body = "<table border=1><tr><td>Kode Toko</td><td>"+kode_toko+"</td></tr><tr><td>IP Public</td><td>"+ip_public+"</td></tr><tr><td>SID</td><td>"+sid+"</td></tr><tr><td>PIC Toko</td><td>NO HP</td></tr></table>"
+	print(id)
+	try:
+		send_mail(
+			'Jaringan Bermasalah',
+			body,
+			'user01@test.com',
+			['user02@test.com'],
+			fail_silently=False,
+			html_message = body,
+			)
+		return HttpResponse('Berhasil di Kirim')
+	except:
+		return HttpResponse('Gagal dikirim')
